22351165b93e6f53466be47635a71ddb91936e61
Irina Saveleva <goyalion17@gmail.com>
2023-02-04 23:27:30 +0100

    create github api

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import { useSearchUsersQuery } from '../store/github/github.api';
 
 export function HomePage() {
+    const {isLoading, isError, data} = useSearchUsersQuery('irinka')
     return (
         <div>home</div>
     )
